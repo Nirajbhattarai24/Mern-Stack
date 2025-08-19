669f49ccd53b176159cf95837046d86bca745d94
Nirajbhattrai24 <bhattarainiraj07@example.com>
2025-08-19 13:52:37 +0545

    continue

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,19 +4,16 @@ import webRoute from "./routes/web.js";
 import connection from "./database/Connection.js";
 import multer from "multer";
 import DatabaseSeeder from "./config/seeder/DatabaseSeeder.js";
+import cors from "cors";
 
 dotenv.config();
 DatabaseSeeder.run();
-
 connection.index();
-
 const PORT = process.env.PORT || 3000;
-
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-
 app.use("/", webRoute);
 
 const URL = process.env.URL;
